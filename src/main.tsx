757f71da94d6e098b77d3aff564839141aeda2c9
ayoub0030 <ayoub.elmeo@gmail.com>
2025-04-25 21:30:47 +0100

    Fix Router import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import './lib/i18n' // Import i18n configuration
 import { AuthProvider } from './hooks/useAuth'
-import { Router } from './lib/router'
+import AppRouter from './lib/router'
 import { queryClient } from './lib/react-query'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <Router />
+        <AppRouter />
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>,
